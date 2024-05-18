1e23b809da1454610425d38035776ed559d08eb1
Jose-Bracalenti <jbracalenti@frsf.utn.edu.ar>
2024-05-18 16:02:28 -0300

    testing

--- src/main/java/gantt/proyecto/POJOS/Actividad.java
+++ src/main/java/gantt/proyecto/POJOS/Actividad.java
@@ -1,5 +1,32 @@
-package gantt.demo.POJOS;
+package gantt.proyecto.POJOS;
 
-public class Tarea {
+import java.time.LocalDate;
+
+import org.hibernate.annotations.Collate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.websocket.ClientEndpoint;
+
+@Entity
+@Table(name = "actividad")
+public class Actividad {
+    @Id
+    private int id;
+    @Column
+    private String nombre;
+    @Column
+    private String descripcion;
+    @Column
+    private LocalDate fecha_inicio;
+    @Column
+    private LocalDate fecha_fin;
+    @Column
+    private float costo;
+    @Column
+    private String resultado_esperado;
+
 
 }
